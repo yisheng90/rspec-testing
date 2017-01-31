972338625095663a92b82a61c15fad50bfd13dcb
yisheng90 <yisheng@yishenglee.com>
2017-01-31 15:35:37 +0800

    Include solution

--- lib/mailbox.rb
+++ lib/mailbox.rb
@@ -1,13 +1,22 @@
 class MailBox
+
+  attr_accessor :letters, :packages
+
   def initialize(letters, packages)
+    @letters = letters
+    @packages = packages
   end
 
   def add_letter
+    self.letters += 1
   end
 
   def add_package
+    self.packages += 1
   end
 
   def clear
+    self.letters = 0
+    self.packages = 0
   end
 end
